bbd02955b7aa21c8302266c1ffdb0568bbcfcbfd
EdmartEmpty <edmart666@gmail.com>
2025-08-01 14:35:48 +0300

    2 commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ["eslint:recommended"],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    
+  },
+};
